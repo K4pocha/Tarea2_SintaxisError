0e00ad8257d82b7cabbfbce08bba8daea8363baa
K4pocha <k4pocha@gmail.com>
2021-05-03 04:59:22 -0400

    Cambios al main, se agrega funcion de lectura y guardado en mapa de datos del csv. Se cambia mapa+lista por MapaHash de laboratorio por comodidad y simplicidad del codigo

---
+++ hashmap.c
@@ -0,0 +1,172 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include "hashmap.h"
+
+typedef struct Pair Pair;
+typedef struct HashMap HashMap;
+int enlarge_called=0;
+
+struct Pair {
+     char * key;
+     void * value;
+};
+
+struct HashMap {
+    Pair ** buckets;
+    long size; //cantidad de datos/pairs en la tabla
+    long capacity; //capacidad de la tabla
+    long current; //indice del ultimo dato accedido
+};
+
+Pair * createPair( char * key,  void * value) {
+    Pair * new = (Pair *)malloc(sizeof(Pair));
+    new->key = key;
+    new->value = value;
+    return new;
+}
+
+long hash( char * key, long capacity) {
+    unsigned long hash = 0;
+     char * ptr;
+    for (ptr = key; *ptr != '\0'; ptr++) {
+        hash += hash*32 + tolower(*ptr);
+    }
+    return hash%capacity;
+}
+
+int is_equal(void* key1, void* key2){
+    if(key1==NULL || key2==NULL) return 0;
+    if(strcmp((char*)key1,(char*)key2) == 0) return 1;
+    return 0;
+}
+
+
+void insertMap(HashMap * map, char * key, void * value) {
+
+  long hashKey = hash(key,map->capacity); //poscicion
+  Pair * dato = createPair(key,value);  //Auxiliar para insertar datos
+
+  while (map->buckets[hashKey] != NULL && map->buckets[hashKey]->key != NULL){ //Validacion (ciclo) que corre mientras buckets tenga datos y que la key exista
+    if(is_equal(map->buckets[hashKey]->key, key) == 1) return; //Caso en que el dato es repetido
+    hashKey = (hashKey + 1) % map->capacity;  
+  }
+  
+  map->buckets[hashKey] = dato;
+  map->size++;
+  if (map->size / map->capacity > 0.70) enlarge(map); //Actualizacion si el mapa se queda sin espacio
+  
+}
+
+void enlarge(HashMap * map) {
+    enlarge_called = 1; //no borrar (testing purposes)
+  
+    Pair ** aux = map->buckets; 
+    long capacity = map->capacity;
+
+    map->capacity=map->capacity*2;  //Aumento de capacidad del mapa
+    map->buckets=(Pair**) calloc (map->capacity, sizeof(Pair *)); //Redireccionando y reasignando memoria de los bukets segun la nueva capacidad
+    long i;
+    for (i = 0; i < capacity; i++){
+      if (aux[i] != NULL)
+      {
+        insertMap(map,aux[i]->key, aux[i]->value); //Insertando buckets con nueva capacidad del mapa
+      }
+    }
+
+
+}
+
+
+HashMap * createMap(long capacity) {
+
+  HashMap * map = (HashMap *)malloc(sizeof(HashMap)); //Creacion y reserva del mapa
+  map->buckets = (Pair **) calloc (capacity, sizeof(Pair *)); //Creacion y reserva pares clave-valor mapa
+  //Iniciado default mapa
+  map->capacity = capacity;   
+  map->size = 0;
+  map->current = -1;
+
+  long i;
+  for (i = 0; i < capacity; i++) {
+    if (map->buckets == NULL) return 0;
+    else
+      map->buckets[i] = NULL;
+  }
+
+  return map;
+}
+
+void eraseMap(HashMap * map,  char * key) {    
+  long hashKey = hash(key,map->capacity); //Buscando posicicon segun funcion hash
+  //long pos = hashKey;
+
+  for ( ; hashKey < map->capacity; hashKey++) //Recorre el mapa hasta ver si el bucket ingresado esta vacio, o hasta que encuentra la key dentro del mapa y la elimina.
+  {
+    if (map->buckets[hashKey] == NULL) break; //Es void, no void *, por lo que no retorna nada --> usar break
+    if(is_equal(map->buckets[hashKey]->key, key) == 1) {
+      map->size--;
+      map->buckets[hashKey]->key = NULL;
+      map->buckets[hashKey]->value = NULL;
+
+    }
+  }
+
+  /** map->buckets[pos]->value = NULL;
+  map->buckets[pos]->key = NULL;
+  map->size--; **/ 
+  
+}
+
+void * searchMap(HashMap * map,  char * key) {   
+  long hashKey = hash(key, map->capacity);
+
+  for ( ; hashKey < map->capacity; hashKey++)  //Recorrido segun la hashKey. Finaliza si es mayor al tamaño del mapa
+  {
+    if (map->buckets[hashKey] != NULL) //Caso en que SI hay datos en esa poscicion del bucket
+    {
+      if (is_equal(map->buckets[hashKey]->key, key))  //Son el mismo dato?
+      {
+        map->current = hashKey;
+        return map->buckets[hashKey]->value;  //Retorna el dato que encontro segun la key
+      }
+  
+    }
+    else if (map->buckets[hashKey] == NULL) return NULL; //No existen datos en ese bucket por lo que no hay que buscar nada.
+  }
+    return NULL;
+}
+
+void * firstMap(HashMap * map) {
+  long i = 0;
+
+
+  while (map->buckets[i] == NULL || map->buckets[i]->key == NULL) /** Intente recorrer con un while(map->capacity < i, y con if cuando eran != a NULL pero no me salio) **/
+  {
+    i++;
+  }
+  map->current = i;
+
+  return map->buckets[i]->value;
+
+  /** map->current = 0;
+ return map->buckets[0]->value; **/
+ return NULL;
+}
+
+void * nextMap(HashMap * map) {
+  long i = map->current;
+  i++;
+  for (; i < map->capacity; i++)  //For que recorrera hasta encontrar un bucket con datos
+  {
+    if (map->buckets[i] != NULL)  //Caso en que el siguiente NO esta vacio
+    {
+      map->current = i;
+      return map->buckets[i]->value;
+    }
+  }
+
+    return NULL; 
+}
---
+++ main_final.c
@@ -0,0 +1,227 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hashmap.h"
+#include <string.h>
+
+
+typedef struct IDPokemon{  //Nodo de la pokedex
+    char nombre[50];//Nombre
+    char tipo[50];//Tipos
+    char prevEvo[50];//Evolucion previa
+    char sigEvo[50];//Evolucion siguiente
+    int numeroPokedex;//Numero en la pokedex
+    char region[50];
+    int cantidadPoke;//Existencias total del pokemon en el almacenamiento
+}Pokedex;
+
+typedef struct Pokemon{ //Variable que va dentro de PokeStorage que es el mapa que almacena los pokemons capturados
+    int id;
+    char nombre[50];
+    int PC;
+    int PS;
+    char genero[50];
+}Pokemon;
+
+const char *get_csv_field (char * tmp, int k) {
+    int open_mark = 0;
+    char* ret=(char*) malloc (100*sizeof(char));
+    int ini_i=0, i=0;
+    int j=0;
+    while(tmp[i+1]!='\0'){
+
+        if(tmp[i]== '\"'){
+            open_mark = 1-open_mark;
+            if(open_mark) ini_i = i+1;
+            i++;
+            continue;
+        }
+
+        if(open_mark || tmp[i]!= ','){
+            if(k==j) ret[i-ini_i] = tmp[i];
+            i++;
+            continue;
+        }
+
+        if(tmp[i]== ','){
+            if(k==j) {
+               ret[i-ini_i] = 0;
+               return ret;
+            }
+            j++; ini_i = i+1;
+        }
+
+        i++;
+    }
+
+    if(k==j) {
+       ret[i-ini_i] = 0;
+       return ret;
+    }
+
+
+    return NULL;
+}
+
+void leerArchivo(HashMap * PokedexID, HashMap * PokedexName, HashMap * almacenamientoPoke, FILE * archivo);    //Lee el csv y almacena datos en los mapas implementados
+void crearPokedex(char * name, char * type, char * prevEvo, char * nextEvo, char * region, int idPokedex, Pokedex * nuevaID);//La variable exist se añade después en la funcion pokeAtrapado
+Pokemon * crearPokemon(int id, int PC, int PS, char * nombre, char * genero);   //Inicializa una estructura nueva, y returna la misma estructura rellenada si corresponde
+void guardarPokedex(HashMap * mapaPokedex, HashMap * PokedexID, Pokedex * nuevoID);
+void mostrarTodoPokedex (HashMap * mapaPokedex);
+
+
+int main()
+{
+    HashMap * mapaPokedex = createMap(200);
+    HashMap * PokedexID = createMap(200);//Almacena PokeID por numero de pokedex
+    HashMap * almacenamientoPoke = createMap(200); //que es lo que tiene dentro?
+    FILE * archivo = fopen("pokemon Archivo1.csv","r");
+    leerArchivo(PokedexID, mapaPokedex, almacenamientoPoke, archivo);
+
+
+
+    /** int op;
+        while(op < 10)
+    {
+        op = 0;
+        printf("1.- Exportar Pokemon\n");
+        printf("2.- Capturar Pokemon\n");
+        printf("3.- Evolucionar Pokemon\n");
+        printf("4.- Buscar Pokemon por Tipo\n");
+        printf("5.- Buscar Pokemon por nombre\n");
+        printf("6.- Buscar por nombre en pokedex\n");
+        printf("7.- Mostrar todos los pokemon de la pokedex\n");
+        printf("8.- Mostrar ordenados por PC\n");
+        printf("9.- Liberar Pokemon\n");
+        printf("10.- Mostrar por region\n");
+        printf("Indica la opcion: ");
+        scanf("%d", &op);
+        fflush(stdin);
+
+        switch(op)
+        {
+            case 1: 
+                leerArchivo(PokedexID, PokedexName, PokeStorage, archivo);
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 4: 
+                break;
+
+            case 5:
+                break;
+
+            case 6: 
+
+                break;
+
+            case 7: 
+
+                break;
+
+            case 8:
+
+                break;
+
+            case 9:
+
+                break;
+
+            case 10: 
+
+                break;
+
+            case 11: 
+                 break;
+        }
+    } **/
+    return 0;
+}
+
+void leerArchivo(HashMap * PokedexID, HashMap * mapaPokedex, HashMap * almacenamientoPoke, FILE * archivo){
+
+    char * linea = (char*)malloc(1024*sizeof(char));
+    char * saltar = (char*)malloc(1024*sizeof(char));
+    Pokedex * nuevaID = (Pokedex *)malloc(sizeof(Pokedex));
+    Pokemon * nuevoPoke = (Pokemon *)malloc(sizeof(Pokemon));
+    saltar = fgets(linea, 100, archivo);
+    int encontrado=0;
+    printf("primera linea = %s", saltar);
+    char * auxkey = (char*)malloc(50*sizeof(char));
+
+    while(fgets (linea, 1023, archivo) != NULL){
+
+       encontrado++;
+       nuevaID->cantidadPoke = 0;
+       nuevoPoke->id = atoi(get_csv_field(linea, 0));//ID
+       strcpy(nuevoPoke->nombre, get_csv_field(linea,1));
+       nuevoPoke->PC = atoi(get_csv_field(linea,3));//PC
+       nuevoPoke->PS = atoi(get_csv_field(linea,4));//PS
+       strcpy(nuevoPoke->genero, get_csv_field(linea,5));//Genero
+       strcpy(nuevaID->nombre, get_csv_field(linea,1));//Nombre
+
+
+       strcpy(nuevaID->tipo, get_csv_field(linea,2));//Tipos
+       itoa(nuevoPoke->id, auxkey, 10);
+       printf("Pokemon numero = %d - Nombre: %s - ID: %d - key aux = ID %s\n", encontrado, nuevoPoke->nombre, nuevoPoke->id, auxkey);
+       strcpy(nuevaID->prevEvo, get_csv_field(linea,6));//Ev. previa
+       strdup(nuevaID->prevEvo);
+       strcpy(nuevaID->sigEvo, get_csv_field(linea,7));//Ev. post
+       strdup(nuevaID->sigEvo);
+       nuevaID->numeroPokedex = atoi(get_csv_field(linea,8));//Num. pokedex
+       strcpy(nuevaID->region, get_csv_field(linea,9));//Region
+       strdup(nuevaID->region);
+
+       insertMap(almacenamientoPoke,(auxkey),nuevoPoke);   //Guardado en el mapa de "almacenamiento"
+       guardarPokedex(mapaPokedex, PokedexID, nuevaID); //Guardado en mapa de pokedex
+       nuevaID = (Pokedex *)malloc(sizeof(Pokedex));
+       nuevoPoke = (Pokemon *)malloc(sizeof(Pokemon));
+    }
+    printf("Archivo leido correctamente");
+}
+
+void crearPokedex(char * nombre, char * tipo, char * prevEvo, char * sigEvo, char * region, int idPokedex, Pokedex * nuevo){
+
+    strcpy(nuevo->nombre, nombre);
+    strcpy(nuevo->sigEvo, sigEvo);
+    strcpy(nuevo->prevEvo, prevEvo);
+    strcpy(nuevo->region, region);
+    strcpy(nuevo->tipo, tipo);
+    nuevo->numeroPokedex = idPokedex;
+}
+
+Pokemon * crearPokemon(int id, int PC, int PS, char * nombre, char * genero){
+    Pokemon * nuevo = (Pokemon *)malloc(sizeof(Pokemon));
+
+    strcpy(nuevo->genero, genero);
+    strcpy(nuevo->nombre, nombre);
+    nuevo->id = id;
+    nuevo->PC = PC;
+    nuevo->PS = PS;
+    return nuevo;
+}
+
+void guardarPokedex(HashMap * mapaPokedex, HashMap * PokedexID, Pokedex * nuevoID){
+    Pokedex * auxPoke;
+    char * auxkey = (char*)malloc(50*sizeof(char));
+
+    if(searchMap(mapaPokedex, nuevoID->nombre)!= NULL){
+        auxPoke = searchMap(mapaPokedex, nuevoID->nombre);
+        auxPoke->cantidadPoke ++;
+        itoa(nuevoID->numeroPokedex, auxkey, 10);
+        auxPoke = searchMap(PokedexID, auxkey);
+        auxPoke->cantidadPoke ++;
+        return;
+
+    }else if(searchMap(mapaPokedex, nuevoID->nombre)== NULL){
+        insertMap(mapaPokedex, nuevoID->nombre, nuevoID);
+        itoa(nuevoID->numeroPokedex, auxkey, 10);
+        insertMap(PokedexID,auxkey, nuevoID);
+        return;
+    }
+}
+
